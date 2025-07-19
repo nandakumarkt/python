2f786e8b6b30ff96165d293a9d43586525232e83
nandakumarkt <nandakumar.kt@gmail.com>
2025-07-19 12:22:26 -0400

    LearningChanges

---
+++ Operators.py
@@ -0,0 +1,65 @@
+#Arithmetic Operator
+# a=int(input("Input the First Number: "))
+# b=int(input("Input the Second Number: "))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+
+#Assignment Operator
+# c=int(input("Input the Number: "))
+# print(c)
+#
+# c+=3
+# print(c)
+#
+# c-=3
+# print(c)
+#
+# c*=3
+# print(c)
+#
+# c**=3
+# print(c)
+
+# Comparison / Relational Operators
+# d=int(input("Input the First Number: "))
+# e=int(input("Input the Second Number: "))
+# print(d==e)
+# print(d>e)
+# print(d<e)
+# print(d>=e)
+
+#logical Operators
+# f=int(input("Input the First Number: "))
+# g=int(input("Input the Second Number: "))
+# # f=True
+# # g=False
+# print(f and g)
+# print(f or g)
+# print(not(f and g))
+
+is_employee = input("Are you an employee? (yes/no): ").lower() == "yes"
+has_access_card = input("Do you have an access card? (yes/no): ").lower() == "yes"
+is_guest = input("Are you a guest? (yes/no): ").lower() == "yes"
+is_blacklisted = input("Are you blacklisted? (yes/no): ").lower() == "yes"
+
+if (is_employee and has_access_card) or (is_guest and not is_blacklisted):
+    print("Access granted.")
+else:
+    print("Access denied.")
+
+
+
+
+
+
+
+
+
+
+
---
+++ datatype.py
@@ -0,0 +1,52 @@
+# x=100
+# y=2.5
+# z=4j
+#
+# print(x)
+# print(y)
+# print(z)
+#
+# a=eval(input("Enter the First number "))
+# b=eval(input("Enter the Second number "))
+# print(a+b)
+
+# a=input("Enter the First number ")
+# b=input("Enter the Second number ")
+# print(int(a) + int(b))
+#print(float(a) + float(b))
+
+
+#list
+# a=[1,2,'Nanda','Prajay']
+# a[0]=1000
+# a.append('Menaka')
+# print(a)
+# print(a[0])
+#
+# my_set={1,2,2,2,3,3,4,4,5,5,5,5}
+# my_set.add(7)
+# my_set.add(6)
+# print(my_set)
+
+#Mapping (Dictionary)
+# mylist={'Name':'Nanda', 'Age' : 30, 'City': 'Toronto'}
+# print(mylist)
+# print(mylist['Name'])
+
+# age=eval(input("Enter you age: "))
+# if age>18:
+#     print("You are an adult")
+# else:
+#     print("You are not an adult - Go to School")
+
+age = int(input("Enter your age: "))
+
+is_adult = age > 18  # Boolean condition stored in a variable
+
+if is_adult:
+    print("You are an adult")
+else:
+    print("You are not an adult - Go to School")
+
+
+
