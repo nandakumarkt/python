a19623e5e414cc6d0070f592bb2b3abacb1889dc
nandakumarkt <nandakumar.kt@gmail.com>
2025-07-19 12:17:51 -0400

    Add script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+# a = 'Nanda'
+# b = 'Prajay'
+# c= 'Son is '+b
+# d= 'Father is '+a
+# print(c)
+# print(d)
+#
+# import keyword
+# print(keyword.kwlist)
+#
+# e=input('Enter Fathers name:')
+# f=input('Enter Mothers name:')
+# print('Husband Name is '+e)
+# print('Wife Name is '+f)
+
+
